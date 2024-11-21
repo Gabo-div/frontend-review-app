f19a377846573c434ac71c88a747eb34d54c4bce
Gabriel Vallés <hernandezvallesgabriel@gmail.com>
2024-11-21 17:22:25 -0400

    feat: token persistente en authStore

--- stores/authStore.tsx
+++ stores/authStore.tsx
@@ -1,6 +1,8 @@
 import { create } from "zustand";
 import axios from "axios";
 import { z } from "zod";
+import { persist, createJSONStorage, StateStorage } from "zustand/middleware";
+import * as SecureStore from "expo-secure-store";
 
 interface AuthStore {
   token: string | null;
@@ -13,56 +15,70 @@ interface AuthStore {
   logout: () => void;
 }
 
-export const useAuthStore = create<AuthStore>((set) => ({
-  token: null,
-  authenticated: false,
-  isLoading: false,
-  login: async ({ username, password }) => {
-    set((state) => ({
-      ...state,
-      isLoading: true,
-    }));
-
-    let success = true;
-
-    try {
-      const data = await axios.post(`${process.env.API_URL}/auth/login`, {
-        username,
-        password,
-      });
-
-      const result = z
-        .object({
-          token: z.string(),
-        })
-        .parse(data.data);
-
-      set({
-        token: result.token,
-        authenticated: true,
-      });
-    } catch (error) {
-      console.log("login error", error);
-
-      set({
-        token: null,
-        authenticated: false,
-      });
-
-      success = false;
-    }
-
-    set((state) => ({
-      ...state,
-      isLoading: false,
-    }));
-
-    return success;
-  },
-  logout: () => {
-    set({
+export const useAuthStore = create(
+  persist<AuthStore>(
+    (set) => ({
       token: null,
       authenticated: false,
-    });
-  },
-}));
+      isLoading: false,
+      login: async ({ username, password }) => {
+        set((state) => ({
+          ...state,
+          isLoading: true,
+        }));
+
+        let success = true;
+
+        try {
+          const data = await axios.post(`${process.env.API_URL}/auth/login`, {
+            username,
+            password,
+          });
+
+          const result = z
+            .object({
+              token: z.string(),
+            })
+            .parse(data.data);
+
+          set({
+            token: result.token,
+            authenticated: true,
+          });
+        } catch (error) {
+          console.log("login error", error);
+
+          set({
+            token: null,
+            authenticated: false,
+          });
+
+          success = false;
+        }
+
+        set((state) => ({
+          ...state,
+          isLoading: false,
+        }));
+
+        return success;
+      },
+      logout: () => {
+        set({
+          token: null,
+          authenticated: false,
+        });
+      },
+    }),
+    {
+      name: "auth-store",
+      storage: createJSONStorage(() => {
+        return {
+          getItem: SecureStore.getItem,
+          setItem: SecureStore.setItem,
+          removeItem: (name: string) => SecureStore.deleteItemAsync(name),
+        } satisfies StateStorage;
+      }),
+    },
+  ),
+);
